d64db02b94089b2ed2418587c841f2473c11203b
Harshavardhan-28 <harshavardhan.khamkar@gmail.com>
2025-05-29 13:45:26 +0530

    Voting tokens contract added

---
+++ VotingTokens.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+ 
+ contract VotingTokens is ERC20 {
+   constructor(
+        string memory _tokenName,
+        string memory _tokenSymbol,
+        uint256 _totalSupply
+   ) ERC20(_tokenName, _tokenSymbol) {
+    _mint(msg.sender, _totalSupply * 10 ** decimals());
+   } 
+
+   mapping(address => uint) public balances;
+
+ }
