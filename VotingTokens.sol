398dd01c8cfe80d4ea8e26b7a1a444b7d88cc6ba
AanchalGupta1162 <aanchalgupta1162@gmail.com>
2025-05-31 19:35:21 +0530

    voting tokens added

--- VotingTokens.sol
+++ VotingTokens.sol
@@ -12,5 +12,26 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
    } 
 
    mapping(address => uint) public balances;
+   function transferTokens(address[] memory members) public {
+    for (uint i = 0; i < members.length ;i++){
+        _mint(members[i], 10 ** decimals());
+        balances[members[i]] = balances[members[i]] + 10 ** decimals();
+    }
+   }
 
+   function transferTokesSingle(address members) public {
+    _mint(members, 10 ** decimals());
+     balances[members] = balances[members] + 10 ** decimals();
+   }
+
+   function burnTokens(address[] memory members,uint256 amount) public{
+        for (uint i = 0; i < members.length ;i++){
+            _burn(members[i], amount ** decimals());
+             balances[members[i]] = balances[members[i]] - amount ** decimals();   
+        }
+   }
+
+   function checkBalance(address user) public view returns (uint) {
+       return balances[user];
+    }
  }
